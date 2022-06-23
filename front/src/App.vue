b620d9fd0645361587f0a80ff9e1f7bf98097a3a
Cedricpasquier <59389978+Dedridec@users.noreply.github.com>
2022-06-23 10:03:30 +0200

    First commit with a bit of front

---
+++ front/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Event />
+</template>
+
+<script>
+import Event from './components/Event.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Event
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ front/src/components/Event.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="event">
+    <div class="form-group">
+      <input class="form-control" placeholder="Event Name" v-model="event.name">
+    </div>
+
+    <div class="form-group">
+      <textarea class="form-control" placeholder="Event Description" v-model="event.description"></textarea>
+    </div>
+
+    <div class="form-group">
+      <input type="date" class="form-control" placeholder="Start Date" v-model="event.start_date">
+    </div>
+
+    <div class="form-group">
+    <input type="date" class="form-control" placeholder="End Date" v-model="event.end_date">
+    </div>
+
+    <button class="btn btn-primary" @click="addEvent">Submit</button>
+  </div>
+  <div class="list_events">
+    <ul id="example-1">
+      <li class="list_group_item"
+          v-for="event in events"
+          :key="event.name"
+      >
+        <h4 class="event_title">
+          {{event.name}}
+        </h4>
+        <h5 class="event_date">
+          {{event.start_date}}
+        </h5>
+        <h5>
+          {{event.end_date}}
+        </h5>
+        <p class="desciption_event">
+          {{event.description}}
+        </p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventItem",
+  data() {
+    return {
+      event: { name: '', description: '', start_date: '', end_date: '' },
+      events: []
+    }
+  },
+  methods: {
+    addEvent: function() {
+      if(this.event.name) {
+        this.events.push(this.event);
+        this.event = { name: '', description: '', start_date: '', end_date: '' }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.field {
+  margin: 0 10px;
+}
+</style>
